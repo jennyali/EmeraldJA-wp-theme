838ae237c6adcc77233fdb437a79374dd4bb95b5
Jennifer Ali <jenny_p_c@hotmail.com>
2017-07-14 10:36:42 +0100

    update

--- functions.php
+++ functions.php
@@ -290,6 +290,37 @@ function wpdocs_my_search_form( $form ) {
 }
 add_filter( 'get_search_form', 'wpdocs_my_search_form' );
 
+// 	Help child categories recognise and go to their parents category-{slug}.php 
+function new_subcategory_hierarchy() { 
+    $category = get_queried_object();
+
+    $parent_id = $category->category_parent;
+
+    $templates = array();
+
+    if ( $parent_id == 0 ) {
+        // Use default values from get_category_template()
+        $templates[] = "category-{$category->slug}.php";
+        $templates[] = "category-{$category->term_id}.php";
+        $templates[] = 'category.php';     
+    } else {
+        // Create replacement $templates array
+        $parent = get_category( $parent_id );
+
+        // Current first
+        $templates[] = "category-{$category->slug}.php";
+        $templates[] = "category-{$category->term_id}.php";
+
+        // Parent second
+        $templates[] = "category-{$parent->slug}.php";
+        $templates[] = "category-{$parent->term_id}.php";
+        $templates[] = 'category.php'; 
+    }
+    return locate_template( $templates );
+}
+
+add_filter( 'category_template', 'new_subcategory_hierarchy' );
+
 
 //----------- AJAX CALLS -------------------//
 
